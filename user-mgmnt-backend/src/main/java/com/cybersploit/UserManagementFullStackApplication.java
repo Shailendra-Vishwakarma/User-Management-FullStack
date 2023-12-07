f3ead564f84797aad27f4e7eeb20cfcfa389d4e2
Shailendra-Vishwakarma <shailendracse1998@gmail.com>
2023-12-07 12:57:04 +0530

    first commit

---
+++ user-mgmnt-backend/src/main/java/com/cybersploit/UserManagementFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.cybersploit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementFullStackApplication.class, args);
+	}
+
+}
